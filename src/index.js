1ce134d706460425648f482cb66f78cb64ece70c
ssenkaayi <ssenkaaayi@gmail.com>
2023-09-30 11:25:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import { HashRouter as Router } from "react-router-dom";
 
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const container =  document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<Router><App /></Router>);
